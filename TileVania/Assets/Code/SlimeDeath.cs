2d84b4852b8388cf62f2548dc3a8404c964122c1
Alexandar Savich <112600558+asavich4@users.noreply.github.com>
2023-07-02 23:32:16 -0400

    New Levels

--- TileVania/Assets/Code/SlimeDeath.cs
+++ TileVania/Assets/Code/SlimeDeath.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SlimeDeath : MonoBehaviour
-{
+public class SlimeDeath : MonoBehaviour{
+
+    [SerializeField] float bounceForce = 5f;
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         PlayerMovement player = collision.collider.GetComponent<PlayerMovement>();
         if (player != null && collision.contacts[0].point.y > transform.position.y)
         {
+            BouncePlayer(player);
             Destroy(transform.parent.gameObject);
         }
+    }
+
+     private void BouncePlayer(PlayerMovement player)
+    {
+        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+        playerRigidbody.velocity = Vector2.up * bounceForce;
     }
 }
 
